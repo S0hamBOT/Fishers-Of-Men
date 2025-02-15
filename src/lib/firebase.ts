579a9313b7428013fb4dd0be3c343ea3e6f146d0
Soham Jadhav <sohamjadhav.tech@gmail.com>
2025-02-15 11:54:47 +0530

    Updated vite.config.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -145,12 +145,12 @@ type ProblemData = {
 };
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBcqf_910XDLwtClN1BSQPx86Y3pU2yK3Y",
+  authDomain: "fishers-of-men-4bf8c.firebaseapp.com",
+  projectId: "fishers-of-men-4bf8c",
+  storageBucket: "fishers-of-men-4bf8c.firebasestorage.app",
+  messagingSenderId: "59588608417",
+  appId: "1:59588608417:web:43a460ba8c56d718858be0",
 };
 
 const app = initializeApp(firebaseConfig);
