28c2cf4c7b6556dba81bed5691a59cf3ad9eea99
Soham Jadhav <sohamjadhav.tech@gmail.com>
2025-02-25 18:43:56 +0530

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,42 @@
+// import path from "path";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"), // <-- Add this
+//     },
+//   },
+//   optimizeDeps: {
+//     exclude: [
+//       /* other packages to exclude, but NOT 'lucide-react' */
+//     ],
+//   },
+// });
+
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy"; // Import the plugin
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      // Add the plugin configuration
+      targets: [
+        {
+          src: path.resolve(__dirname, "src/photos"),
+          dest: "./photos", // Copies to the 'photos' folder in dist
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // <-- Add this
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   optimizeDeps: {
