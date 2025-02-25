c76ac3c97fbe4ef192f314407b59dc78082bb3a4
Soham Jadhav <sohamjadhav.tech@gmail.com>
2025-02-25 18:52:46 +0530

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,17 +19,16 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy"; // Import the plugin
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
     react(),
     viteStaticCopy({
-      // Add the plugin configuration
       targets: [
         {
           src: path.resolve(__dirname, "src/photos"),
-          dest: "./photos", // Copies to the 'photos' folder in dist
+          dest: "./", // Change dest to the root of dist
         },
       ],
     }),
